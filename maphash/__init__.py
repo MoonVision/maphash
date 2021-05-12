74cf1a0426890f6a4635e3be375d9ee396b06ea1
Jakob Klepp <jakob.klepp@moonvision.io>
2021-05-12 15:05:05 +0200

    Implement maphash & json_dump

--- maphash/__init__.py
+++ maphash/__init__.py
@@ -1,1 +1,25 @@
 __version__ = '0.1.0'
+
+import hashlib
+import json
+from typing import Union, Dict, List
+
+
+Serializable = Union[Dict, List, str, int, float]
+
+
+def json_dump(obj: Serializable) -> str:
+    """"""
+    return json.dumps(
+        obj,
+        indent=2,
+        sort_keys=True,
+    )
+
+
+def maphash(obj: Serializable) -> str:
+    """"""
+    return hashlib.sha3_256(
+        json_dump(obj),
+    ).hexdigest()
+
