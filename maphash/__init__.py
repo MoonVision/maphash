0eb80c84e3552a597f653b45ddeaab93245b8504
Jakob Klepp <jakob.klepp@moonvision.io>
2021-05-12 15:08:38 +0200

    Add pre-commit config

--- maphash/__init__.py
+++ maphash/__init__.py
@@ -1,25 +1,17 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 import hashlib
 import json
-from typing import Union, Dict, List
-
+from typing import Dict, List, Union
 
 Serializable = Union[Dict, List, str, int, float]
 
 
 def json_dump(obj: Serializable) -> str:
     """"""
-    return json.dumps(
-        obj,
-        indent=2,
-        sort_keys=True,
-    )
+    return json.dumps(obj, indent=2, sort_keys=True,)
 
 
 def maphash(obj: Serializable) -> str:
     """"""
-    return hashlib.sha3_256(
-        json_dump(obj),
-    ).hexdigest()
-
+    return hashlib.sha3_256(json_dump(obj),).hexdigest()
